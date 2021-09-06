3f08670b30bd3407928429722d363bf46c286487
Jessica Nelson <yahwehschica@yahoo.com>
2021-09-06 12:04:26 -0400

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,24 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+introduction("Josh")
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguage("Jessica", "Javascript")
+
+function introductionWithLanguageOptional(name, language = "Javascript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Gracie", "Python")
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Gracie")
+
+
+
+
+
